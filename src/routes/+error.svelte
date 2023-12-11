719d22c7fb372e5b80e9fa339a254a1294ad6345
happysalada <raphael@megzari.com>
2023-12-11 13:50:07 +0900

    deps: update

---
+++ src/routes/+error.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { page } from '$app/stores';
+</script>
+
+<!--
+  This example requires updating your template:
+
+  ```
+  <html class="h-full">
+  <body class="h-full">
+  ```
+-->
+<main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8 h-screen">
+  <div class="text-center">
+    <p class="text-base font-semibold text-primary-600">{$page.status}</p>
+    <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Error</h1>
+    <p class="mt-6 text-base leading-7 text-gray-600">
+      {$page.error?.message || 'No error message'}
+    </p>
+    <div class="mt-10 flex items-center justify-center gap-x-6">
+      <a
+        href="/"
+        class="rounded-md bg-primary-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
+        >Go back home</a
+      >
+      <a href="mailto:admin@example.com" class="text-sm font-semibold text-gray-900"
+        >Contact support <span aria-hidden="true">&rarr;</span></a
+      >
+    </div>
+  </div>
+</main>
